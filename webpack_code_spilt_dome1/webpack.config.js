24734a6f5007e616f3cd57379818fdc7039f8dad
Rascal <2441940130@qq.com>
2024-03-09 13:31:11 +0800

    push

--- webpack_code_spilt_dome1/webpack.config.js
+++ webpack_code_spilt_dome1/webpack.config.js
@@ -1,2 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    mode:'production',
+    entry:{
+        main:'./src/main.js',
+        app:'./src/app.js'
+    },
 
+    output:{
+        path:  path.resolve(__dirname,'dist'), //所有文件输出目录
+        //文件名
+        filename: '[name].js',
+    },
 
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,'./public/index.html')
+        })
+    ]
+}
